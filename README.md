1261cd578dc27cb4e35482152a95eefeb71e5aaf
Endscape-Coding <mishenka228339@gmail.com>
2026-02-26 02:52:08 +0300

    Added config manager, check user id work with .env files. Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # EN-OS-Remote-Assistant
 Control your PC via Telegram
+
+How to build?
+
+1: Create .env file
+2: Write to env file: 
+```
+TELOXIDE_TOKEN=YOUR-TOKEN
+ID=0000000000 (Your Telegram id)
+```
+
+3: Exec cargo build
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
+use std::env;
 use teloxide::prelude::*;
+use teloxide::utils::command::BotCommands;
+use teloxide::dispatching::Dispatcher;
 
 mod handlers;
 
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+enum Botcommand {
+    #[command(description = "Run command")]
+    Cmd(String),
+
+    #[command(description = "Set sudo password")]
+    Sudo(String),
+
+    Start,
+
+    Help,
+}
+
 #[tokio::main]
 async fn main() {
-    println!("EN-OS Remote Assistant creator");
+    dotenvy::dotenv().expect("Failed foad from .env file");
+    println!("EN-OS Remote Assistant");
     pretty_env_logger::init();
-    log::info!("Starting bot");
-    let bot = Bot::new("7776624127:AAHjL9T8PJw4myCeqpGaUVZnts1FXWQU5jY");
-    teloxide::repl(bot, |bot, msg| {
-    async move {
-        handlers::message(bot, msg).await?;
-        Ok(())
-       }
-    }).await;
+    log::info!("Starting bot...");
+    let bot = Bot::from_env();
+
+    let id: i64 = env::var("ID").expect("Error").parse::<i64>().expect("Error parce your id");
+    handlers::config_read;
+
+    let handler = dptree::entry()
+    .branch(
+        Update::filter_message()
+        .filter_command::<Botcommand>()
+        .filter(move |msg:Message| {
+            msg.chat.id == ChatId(id)
+        })
+        .branch(dptree::case![Botcommand::Cmd(args)].endpoint(handlers::cmd))
+        .branch(dptree::case![Botcommand::Sudo(args)].endpoint(handlers::sudo))
+        .branch(dptree::case![Botcommand::Start].endpoint(handlers::start))
+        .branch(dptree::case![Botcommand::Help].endpoint(handlers::start))
+    );
+
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
 }
